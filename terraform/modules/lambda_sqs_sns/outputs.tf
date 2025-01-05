d07ab51f46fc9494ee5cf5e8c74b366b93e461e2
Yas-CF <yasna.kcf@gmail.com>
2025-01-05 01:38:22 -0300

    Create outputs.tf

---
+++ terraform/modules/lambda_sqs_sns/outputs.tf
@@ -0,0 +1,19 @@
+output "lambda_function_name" {
+  description = "Nombre de la función Lambda"
+  value       = aws_lambda_function.lambda_function.function_name
+}
+
+output "sqs_queue_url" {
+  description = "URL de la cola SQS"
+  value       = aws_sqs_queue.sqs_queue.id
+}
+
+output "sns_topic_arn" {
+  description = "ARN del tema SNS"
+  value       = aws_sns_topic.sns_topic.arn
+}
+
+output "sqs_queue_arn" {
+  description = "ARN de la cola SQS"
+  value       = aws_sqs_queue.sqs_queue.arn
+}
