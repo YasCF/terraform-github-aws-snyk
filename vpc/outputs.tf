49239935c6c6cd3c441e7d791a0dbef5f2230c1a
Yas-CF <yasna.kcf@gmail.com>
2025-01-05 14:10:16 -0300

    Delete vpc directory

--- vpc/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vpc_id" {
-  description = "ID de la VPC creada"
-  value       = aws_vpc.main.id
-}
-
-output "public_subnet_id" {
-  description = "ID de la subred pública creada"
-  value       = aws_subnet.public.id
-}
